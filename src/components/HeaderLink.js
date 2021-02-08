359d3a33c0c6b106e28469e2569673be189a6b6f
Markus Tuominen <maketuominen@hotmail.com>
2021-02-08 20:42:17 +0200

    Header link styling

--- src/components/HeaderLink.js
+++ src/components/HeaderLink.js
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom"
 
 function HeaderLink(props) {
     return (
-        <Link className="App-header-link" to={props.to}>{props.name}</Link>
+        <Link className="App-header-link" to={props.to}>
+            {props.name}
+        </Link>
     )
 }
 
