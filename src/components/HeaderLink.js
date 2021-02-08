3bece603dfdc68b645c5a5e17549c21d34707c7a
Markus Tuominen <maketuominen@hotmail.com>
2021-02-08 17:58:56 +0200

    Added react-router Initial changes

---
+++ src/components/HeaderLink.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+function HeaderLink(props) {
+    return (
+        <Link className="App-header-link" to={props.to}>{props.name}</Link>
+    )
+}
+
+export default HeaderLink
