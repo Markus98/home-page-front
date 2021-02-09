85c6875245d533f34e727c22b19e6a4d9a85afff
Markus Tuominen <maketuominen@hotmail.com>
2021-02-10 01:17:00 +0200

    Moved header to separate component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import './App.css';
-import HeaderLink from './components/HeaderLink';
-import DarkToggle from './components/DarkToggle';
-import {
-  BrowserRouter as Router,
-  Switch, Route, Link, useLocation, useParams
-} from "react-router-dom"
-import { useEffect, useState } from 'react';
+import Header from './components/Header'
 
 function App() {
-  const pathname = useLocation().pathname;
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="App-header-left">
-
-        </div>
-        <div className="App-header-content">
-          <HeaderLink name="Home" to="/" location={pathname} />
-          <HeaderLink name="Projects" to="/projects" location={pathname} />
-          <HeaderLink name="Blog" to="/blog" location={pathname} />
-        </div>
-        <div className="App-header-right">
-          <DarkToggle/>
-        </div>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import './Header.css';
+import HeaderLink from './HeaderLink';
+import DarkToggle from './DarkToggle';
+import {useLocation} from "react-router-dom";
+
+function Header() {
+    const pathname = useLocation().pathname;
+
+    return (
+        <header className="App-header">
+            <div className="App-header-left">
+
+            </div>
+            <div className="App-header-content">
+                <HeaderLink name="Home" to="/" location={pathname} />
+                <HeaderLink name="Projects" to="/projects" location={pathname} />
+                <HeaderLink name="Blog" to="/blog" location={pathname} />
+            </div>
+            <div className="App-header-right">
+                <DarkToggle />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
