eaadff2f9939ed676ac10468301be6d6331db1e4
Markus Tuominen <maketuominen@hotmail.com>
2021-03-06 18:11:21 +0200

    little bit of thumbnail styling

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,13 +1,16 @@
 import './ArticleCard.css';
 import { Link } from 'react-router-dom';
 
-function ArticleCard({ title, abstract, id }) {
+function ArticleCard({ title, abstract, id, thumbnail }) {
 
     return (
         <Link className="articleCardLink" to={"/projects/" + id}>
             <div className="articleCard">
-                <h2 className="articleCardTitle">{title}</h2>
-                <div className="articleCardAbstract">{abstract}</div>
+                <img className="articleCardThumbnail" src={thumbnail} />
+                <div className="articleCardTextArea">
+                    <h2 className="articleCardTitle">{title}</h2>
+                    <div className="articleCardAbstract">{abstract}</div>
+                </div>
             </div>
         </Link>
     );
--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -18,7 +18,7 @@ function ProjectsPage() {
         <div className="ContentTextArea">
             <h1>This is the home of my projects.</h1>
             {projectCards.map(card => 
-                <ArticleCard id={card.id} title={card.title} abstract={card.abstract} />
+                <ArticleCard id={card.id} title={card.title} abstract={card.abstract} thumbnail={card.thumbnail} />
             )}
         </div>
     )
