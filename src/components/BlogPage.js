74f74c24aeeed434302fd70f7dd0188c7c19e297
Markus Tuominen <maketuominen@hotmail.com>
2021-03-10 17:44:16 +0200

    Added a back button to blog pages

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import "./BlogPage.css";
+import { Link } from 'react-router-dom';
+
 
 
 function BlogPage({ apipath }) {
@@ -16,7 +19,9 @@ function BlogPage({ apipath }) {
 
     return (
         <div className="App-content">
-            <div className="App-content-side"></div>
+            <div className="App-content-side" id="left">
+                <Link to="/projects" id="back-button"> <i class="arrow"></i> Back</Link>
+            </div>
             <div className="App-content-area">
                 <div className="ContentTextArea" dangerouslySetInnerHTML={{__html: postContent}} />
             </div>
